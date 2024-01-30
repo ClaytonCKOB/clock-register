496f8a99f74e629e06984ec573ea9d3ea14f96b7
ClaytonCKOB <claytoncaco89@gmail.com>
2024-01-30 14:09:06 -0300

    initial commit

---
+++ src/main/java/org/ckob/clock_register/ClockRegisterApplication.java
@@ -0,0 +1,13 @@
+package org.ckob.clock_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ckob/clock_register/ClockRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ckob.clock_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
